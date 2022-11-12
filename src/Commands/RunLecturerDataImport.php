97d7fb3965f91b4b00f2285633b0df7f419285d3
Antonio Bruno <abruno@artiso.com>
2022-11-12 11:36:45 +0100

    reformat code

--- src/Commands/RunLecturerDataImport.php
+++ src/Commands/RunLecturerDataImport.php
@@ -13,71 +13,70 @@ use UnipvLecturers\Spiders\UniPvEngineeringClassesSpider;
 use UnipvLecturers\Spiders\UniPvEngineeringCoursesSpider;
 use UnipvLecturers\Spiders\UniPvEngineeringTeacherEmailSpider;
 
-class RunLecturerDataImport extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected string $signature = 'unipv:lecturer-import {--R|refresh}';
+class RunLecturerDataImport extends Command {
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected string $signature = 'unipv:lecturer-import {--R|refresh}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected string $description = 'Run spiders to get data of courses from engineering courses..';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected string $description = 'Run spiders to get data of courses from engineering courses..';
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle(): int {
+	/**
+	 * Execute the console command.
+	 *
+	 * @return int
+	 */
+	public function handle(): int {
 
-	    $refreshOption = $this->argument('refresh');
+		$refreshOption = $this->argument( 'refresh' );
 
 		$skipCleaningOperation = $refreshOption;
 
-	    $this->execute('cleaning DB', fn () => $this->truncateTables() , $skipCleaningOperation);
+		$this->execute( 'cleaning DB', fn() => $this->truncateTables(), $skipCleaningOperation );
 
-		$this->execute('import classes', fn () => Roach::startSpider(UniPvEngineeringClassesSpider::class) );
+		$this->execute( 'import classes', fn() => Roach::startSpider( UniPvEngineeringClassesSpider::class ) );
 
-		$this->execute('import teacher email', fn () => Roach::startSpider(UniPvEngineeringTeacherEmailSpider::class));
+		$this->execute( 'import teacher email', fn() => Roach::startSpider( UniPvEngineeringTeacherEmailSpider::class ) );
 
-	    $this->execute('import lecturers', fn () => Roach::startSpider(UniPvEngineeringCoursesSpider::class));
+		$this->execute( 'import lecturers', fn() => Roach::startSpider( UniPvEngineeringCoursesSpider::class ) );
 
-        return Command::SUCCESS;
-    }
+		return Command::SUCCESS;
+	}
 
-	private function execute(string $operation, Closure $operationCallback, bool $skip = false): void{
+	private function execute( string $operation, Closure $operationCallback, bool $skip = false ): void {
 
-		if ($skip) {
+		if ( $skip ) {
 
-			$this->info("SKIPPING | $operation.....");
+			$this->info( "SKIPPING | $operation....." );
 
 			return;
 		}
 
-		$this->info("START | $operation.....");
+		$this->info( "START | $operation....." );
 
 		$operationCallback();
 
-		$this->info("END | $operation.....");
+		$this->info( "END | $operation....." );
 	}
 
-    private function truncateTables(){
+	private function truncateTables() {
 
-        TeacherLesson::removeConstraints();
-        Lecturer::removeConstraints();
+		TeacherLesson::removeConstraints();
+		Lecturer::removeConstraints();
 
-        TeacherLesson::query()->truncate();
-        Lecturer::query()->truncate();
-        Lesson::query()->truncate();
-        Teacher::query()->truncate();
+		TeacherLesson::query()->truncate();
+		Lecturer::query()->truncate();
+		Lesson::query()->truncate();
+		Teacher::query()->truncate();
 
-        Lecturer::addConstraints();
-        TeacherLesson::addConstraints();
-    }
+		Lecturer::addConstraints();
+		TeacherLesson::addConstraints();
+	}
 }
